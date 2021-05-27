d2b2515635b5f715523d87db3d33211b4504e4c7
lianuva <lianjongejan@hotmail.com>
2021-05-27 21:25:18 +0200

    heroku

--- Instructions/__init__.py
+++ Instructions/__init__.py
@@ -25,12 +25,12 @@ class Player(BasePlayer):
         widget=widgets.RadioSelect,
         label="By clicking the button below, you acknowledge that your participation in the study is voluntary, you are over 18 years of age, and that you are aware that you may choose to terminate your participation in the study at any time and for any reason.",
     )  
-    # os1 = models.StringField(blank=True)
+    os1 = models.StringField(blank=True)
 
 # PAGES
 class Welcome(Page):
     form_model = 'player'
-    # form_fields = ['consent', 'os1']
-    form_fields = ['consent']
+    form_fields = ['consent', 'os1']
+    # form_fields = ['consent']
 
 page_sequence = [Welcome]
